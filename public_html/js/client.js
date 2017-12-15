d17e5b0f4f0fd5c82c9c2c5ab04ec779f88847be
catDidenko <yekaterina.didenko@codeit.com.ua>
2017-12-15 19:45:18 +0200

    hdihoisg

--- public_html/js/client.js
+++ public_html/js/client.js
@@ -30,7 +30,7 @@ ArticleController.prototype.addArticle = function(title, content, author){
             content: content,
             author: author
         }
-    }).done(this.getAllArticles);
+    }).done(this.indexAction);
 }
 
 ArticleController.prototype.deleteArticle = function(id){
@@ -53,27 +53,9 @@ ArticleController.prototype.editArticle = function(id,title, content, author){
         data: {
             title:title,
             content: content,
-            author: author
+            author: author,
         }
-        }).done(this.getAllArticles);
-}
-
-ArticleController.prototype.addForm = function(){
-     output += '<tr id="'+ data[key].id +'">';
-        output += '<td>' + data[key].id + '</td>';
-        output += '<td>' + data[key].title + '</td>';
-        output += '<td>' + data[key].content + '</td>';
-        output += '<td>' + data[key].author + '</td>';
-        output += '<td>' + data[key].creation_date + '</td>';
-        output += '<td>'+
-                  '<button type="button" class="material-icons" onclick="articles.editAction('+data[key].id+')">edit</button>' +
-                  '<button type="button" class="material-icons" onclick="articles.deleteAction('+data[key].id+')">delete</button>' +
-                  '</td>';
-        output += '</tr>';
-}
-
-ArticleController.prototype.editForm = function(){
-
+        }).done(this.indexAction);
 }
 
 ArticleController.prototype.viewTable = function(data){
@@ -84,15 +66,15 @@ ArticleController.prototype.viewTable = function(data){
         output += '<td>' + data[key].title + '</td>';
         output += '<td>' + data[key].content + '</td>';
         output += '<td>' + data[key].author + '</td>';
-        output += '<td>' + data[key].creation_date + '</td>';
+        output += '<td>' + articles.UpdateTime(data[key].creation_date) + '</td>';
         output += '<td>'+
                   '<button type="button" class="material-icons" onclick="articles.editAction('+data[key].id+')">edit</button>' +
                   '<button type="button" class="material-icons" onclick="articles.deleteAction('+data[key].id+')">delete</button>' +
                   '</td>';
         output += '</tr>';
     }
-    articles.posts = data;
     $('#articles').html(output);
+    articles.posts = data;
 }
 
 ArticleController.prototype.Search = function(){
@@ -106,7 +88,7 @@ ArticleController.prototype.Search = function(){
                 output += '<td>' + value.title + '</td>';
                 output += '<td>' + value.content + '</td>';
                 output += '<td>' + value.author + '</td>';
-                output += '<td>' + value.creation_date + '</td>';
+                output += '<td>' + articles.UpdateTime(value.creation_date) + '</td>';
                 output += '<td>'+
                   '<button type="button" class="material-icons" onclick="articles.editAction('+value.id+')">edit</button>' +
                   '<button type="button" class="material-icons" onclick="articles.deleteAction('+value.id+')">delete</button>' +
@@ -117,11 +99,35 @@ ArticleController.prototype.Search = function(){
     $('#articles').html(output);
 }
 
+ArticleController.prototype.UpdateTime = function(creation_date){
+
+    var seconds = Math.floor(((new Date().getTime()/1000) - creation_date)),
+    interval = Math.floor(seconds / 31536000);
+
+    if (interval > 1) return interval + "yers ago";
+
+    interval = Math.floor(seconds / 2592000);
+    if (interval > 1) return interval + " month ago";
+
+    interval = Math.floor(seconds / 86400);
+    if (interval >= 1) return interval + " days ago";
+
+    interval = Math.floor(seconds / 3600);
+    if (interval >= 1) return interval + " hours ago";
+
+    interval = Math.floor(seconds / 60);
+    if (interval > 1) return interval + " minutes ago";
+
+    return Math.floor(seconds) + " seconds ago";
+}
+
 
 function ArticleController() {
     this.posts;
     this.indexAction = function() {
-        this.getAllArticles();
+        $('#addArcticle').hide();
+        $('#articleTable').show();
+        articles.getAllArticles();
     };
 
     this.addAction = function() {
@@ -132,7 +138,7 @@ function ArticleController() {
             content = $('#content').val();
             author = $('#author').val();
             articles.addArticle(title, content, author);
-        })
+        });
     };
 
     this.deleteAction = function(id) {
@@ -148,19 +154,41 @@ function ArticleController() {
         $('#content').val("" + article.cells[2].innerHTML);
         $('#author').val("" + article.cells[3].innerHTML);
         id = article.cells[0].innerHTML;
-    
         $('#add').click(function(){
             title = $('#title').val();
             content = $('#content').val();
             author = $('#author').val();
             articles.editArticle(id, title, content, author);
-        })
+        });
     };
 }
 
 var articles = new ArticleController();
 articles.indexAction();
-$('#addArcticle').hide();
+
+$(".sortable").click(function(){
+    var o = $(this).hasClass('asc') ? 'desc' : 'asc';
+    $('.sortable').removeClass('asc').removeClass('desc');
+    $(this).addClass(o);
+
+    var colIndex = $(this).prevAll().length;
+    var tbod = $(this).closest("table").find("tbody");
+    var rows = tbod.find("tr");
+
+    rows.sort(function(a,b){
+        var A = $(a).find("td").eq(colIndex).text();
+        var B = $(b).find("td").eq(colIndex).text();
+
+        if (!isNaN(A)) A = Number(A);
+        if (!isNaN(B)) B = Number(B);
+
+        return o == 'asc' ? A > B : B > A;
+    });
+
+    $.each(rows, function(index, ele){
+        tbod.append(ele);
+    });
+});
 
 
 
